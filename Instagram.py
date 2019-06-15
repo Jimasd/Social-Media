52ffb04b77bfb42e3decc4bd7dbdc8c2dfa8ae99
Jimasd <kolpeanut@gmail.com>
2019-06-15 14:06:32 -0400

    Instagram: Initial commit

--- Instagram.py
+++ Instagram.py
@@ -1,17 +1,19 @@
 from Secret import get_instagram_info, get_instagram_URL
-
 import requests
+import pandas as pd
 
 CLIENT_ID, CLIENT_SECRET, REDIRECT_URI = get_instagram_info()
-CODE = '' #get_instagram_URL()
+CODE = input("Manually input the code after '?code=...' in\n{}\nHere: ".format(get_instagram_URL()))
 
 payload = dict(client_id=CLIENT_ID,
                 client_secret=CLIENT_SECRET,
                 grant_type='authorization_code',
                 redirect_uri=REDIRECT_URI,
                 code=CODE)
-response = requests.post('https://api.instagram.com/oauth/access_token', data = payload)
-print(response.json())
 
+response = requests.post('https://api.instagram.com/oauth/access_token', data = payload)
 ACCESS_TOKEN = response.json()['access_token']
+
+print(pd.DataFrame(response.json())["user"])
 print(ACCESS_TOKEN)
+print(CODE)
