8405ea8fa5b77d1870c7d36a47773efce4f70fa2
Jimasd <kolpeanut@gmail.com>
2019-06-15 11:06:58 -0400

    Facebook: Initial commit

---
+++ Facebook.py
@@ -0,0 +1,18 @@
+from Secret import get_fb_access_token
+
+import facebook
+import json
+
+ACCESS_TOKEN = get_fb_access_token()
+
+def pretty_print(o):
+    print(json.dumps(o, indent=1))
+
+fb_graph =  facebook.GraphAPI(ACCESS_TOKEN)
+
+#pretty_print(fb_graph.get_object("me"))
+#pretty_print(fb_graph.get_connections(id="me", connection_name="likes"))
+pretty_print(fb_graph.request("search", {"type": "place",
+                                         "center": "40.749444, 73.968056",
+                                         "fields": "name, location"
+                                         }))
